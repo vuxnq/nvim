32216fec24e742743ec4778483752c3b42df5aab
vuxnq <vuxnqu@gmail.com>
2025-12-16 16:45:59 +0100

    commit it

--- init.lua
+++ init.lua
@@ -16,11 +16,9 @@ require("lazy").setup({
     require("plugins.colortheme"),
 	require("plugins.bufferline"),
 	require("plugins.lualine"),
-	require("plugins.treesitter"),
 	require("plugins.telescope"),
-	-- require("plugins.lsp"),
-	require("plugins.autocomplete"),
-	-- require("plugins.autoformat"),
+	require("plugins.lsp"),
+	require("plugins.completion"),
 
 	{ "folke/which-key.nvim" },
 })
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,6 +11,8 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-s>", "<cmd>w <CR>", opts)
 vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
 
+vim.keymap.set("n", "<Esc>", ":noh<CR>", opts)
+
 -- zmrde necpi mi to do registeru
 vim.keymap.set("n", "x", '"_x', opts)
 
