7784b7f171e654f4e776b2f34ed66df8daf4c89d
vuxnq <vuxnqu@gmail.com>
2025-12-16 01:31:25 +0100

    neco pribylo

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,34 @@
+-- leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- conciseness
+local opts = { noremap = true, silent = true }
+
+-- yeaaaa
+vim.keymap.set("n", "<C-s>", "<cmd>w <CR>", opts)
+vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
+
+-- zmrde necpi mi to do registeru
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- resize
+vim.keymap.set("n", "<Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+
+-- buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts)
+vim.keymap.set("n", "<leader>n", "<cmd>enew<CR>", opts)
+
+-- linewrap
+vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
+
+-- stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
---
+++ lua/core/options.lua
@@ -0,0 +1,14 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.linebreak = true
+o.autoindent = true
+o.ignorecase = true
+o.smartcase = true
+
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
