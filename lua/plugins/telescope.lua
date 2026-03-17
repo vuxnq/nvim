7182d394579eab82cd36696c11dd98ff93281092
vuxnq <vuxnqu@gmail.com>
2026-03-17 02:46:51 +0100

    awidoawidh

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,14 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>ff', function ()
+            builtin.find_files({ hidden = true })
+        end, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', function ()
+            builtin.live_grep({additional_args = function ()
+                return { "--hidden" }
+            end})
+        end, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end,
